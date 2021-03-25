9701a53f9edf1b6d5ec817e184ab4749334f3078
Youness <oubelkacemy@gmail.com>
2021-03-25 20:36:41 +0100

    landing page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/index.css">
+  <title>Watch It</title>
+</head>
+<body>
+  <header class="header">
+    <h1 class="title">Watch It</h1>
+    <p class="description">Keep track of your favorite Movies and Tv Shows</p>
+  </header>
+  <main class="login-signup">
+    <button class="btn"><a href="#">Log-in</a></button>
+    <button class="btn"><a href="#">Sign Up</a></button>
+  </main>
+  
+</body>
+</html>
